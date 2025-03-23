2de43a9046ac6405a1bc3fe6e9a7435617d0a1ff
Miguel Lopez <mlopez514@unab.edu.co>
2025-03-22 20:05:40 -0500

    Actualiza app.py con [describe brevemente los cambios]

--- app.py
+++ app.py
@@ -179,6 +179,8 @@ def deshacer_movimiento(tipo, mov_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    import os
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
