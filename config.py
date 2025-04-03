940512c289070ef480c1b2516342fecd594425f7
Miguel Lopez <mlopez514@unab.edu.co>
2025-04-03 12:31:53 -0500

    Actualización de archivos

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'super-secret-key')
-    PERMANENT_SESSION_LIFETIME = timedelta(minutes=15)
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=5)
     SEND_FILE_MAX_AGE_DEFAULT = 0
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL',
